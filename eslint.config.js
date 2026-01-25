c0caaac554236c0228c213e2206b178d13ab2c8b
yomi <yinkom0708@gmail.com>
2026-01-25 19:03:14 +0900

    Update: Release v1.0.1

--- eslint.config.js
+++ eslint.config.js
@@ -2,23 +2,38 @@ import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import json from '@eslint/json';
-import { defineConfig } from 'eslint/config';
 
-export default defineConfig({
-  ignores: ['dist/'],
-  overrides: [
-    {
-      files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-      plugins: { js },
-      extends: ['js/recommended'],
-      languageOptions: { globals: globals.node },
+export default [
+  {
+    ignores: ['dist/**'],
+  },
+
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      globals: globals.node,
     },
-    Object.assign({ files: ['**/*.{ts,mts,cts}'] }, tseslint.configs.recommended),
-    {
-      files: ['**/*.json'],
-      plugins: { json },
-      language: 'json/json',
-      extends: ['json/recommended'],
+    ...js.configs.recommended,
+  },
+
+  {
+    files: ['**/*.{ts,mts,cts}'],
+    languageOptions: {
+      globals: globals.node,
+      parser: tseslint.parser,
     },
-  ],
-});
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+    },
+  },
+
+  {
+    files: ['**/*.json'],
+    language: 'json/json',
+    plugins: { json },
+    ...json.configs.recommended,
+  },
+];
