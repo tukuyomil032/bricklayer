8dbd3c0244384550f622f499fc4e8606755fb051
tukuyomi032 <118542180+tukuyomil032@users.noreply.github.com>
2026-03-30 11:30:33 +0900

    chore: bump version to 1.0.45

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const program = new Command();
 program
   .name('brick')
   .description('Interactive CLI to scaffold TypeScript CLI projects')
-  .version('1.0.6');
+  .version('1.0.45');
 
 program.addCommand(createCommand());
 program.addCommand(sampleCommand());
